3a8ceef9eb4967bf09414d89e9af3df43d4c07f1
BLARD <justin.blard@epitech.eu>
2022-03-19 19:31:05 +0000

    [UPD] Adding mod Gundam: Iron-Blooded Orphans Mobile Suits
    The mod listed above as been added to the game.
    Element have been changed and removed.
    All ship variant with MS have been removed.
    Faction owning certain stuff is absent.
    av_system as been reimplmented.

---
+++ data/hullmods/AlayaVijnanaSystem.java
@@ -0,0 +1,26 @@
+package data.hullmods;
+
+import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+
+public class AlayaVijnanaSystem extends BaseHullMod {
+
+  public static final float MANEUVER_BONUS = 50.0f;
+
+  public void applyEffectsBeforeShipCreation(
+    HullSize hullSize,
+    MutableShipStatsAPI stats,
+    String id
+  ) {
+    stats.getAcceleration().modifyPercent(id, MANEUVER_BONUS * 2f);
+    stats.getDeceleration().modifyPercent(id, MANEUVER_BONUS);
+    stats.getTurnAcceleration().modifyPercent(id, MANEUVER_BONUS * 2f);
+    stats.getMaxTurnRate().modifyPercent(id, MANEUVER_BONUS);
+  }
+
+  public String getDescriptionParam(int index, HullSize hullSize) {
+    if (index == 0) return "" + (int) MANEUVER_BONUS + "%";
+    return null;
+  }
+}
