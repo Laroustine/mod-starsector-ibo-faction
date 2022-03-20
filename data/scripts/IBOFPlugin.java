e6cef2bac0807774687e476cb6d24213117f18e1
BLARD <justin.blard@epitech.eu>
2022-03-20 22:39:46 +0000

    [ADD] Removable faction option
    Factions can be deactivated from spawnning in the game world.

--- data/scripts/IBOFPlugin.java
+++ data/scripts/IBOFPlugin.java
@@ -8,27 +8,15 @@
 
 package data.scripts;
 
-import java.io.IOException;
-
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import data.scripts.world.IBOFGen;
 
 public class IBOFPlugin extends BaseModPlugin {
 
-  private static final String SETTINGS_FILE = "options.ini";
-  private static JSONObject settings;
-
   private static void initIBOF() {
     new IBOFGen().generate(Global.getSector());
-  }
-
-  private static JSONObject loadSettings() throws IOException, JSONException {
-    return Global.getSettings().loadJSON(SETTINGS_FILE);
   }
 
   @Override
