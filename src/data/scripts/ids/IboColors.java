aec4488bc6391ebb4305aaf8b6cafe697e0eff0a
Laroustine <justin.blard@epitech.eu>
2022-07-27 19:32:20 +0100

    [UPD] Move script bits

---
+++ src/data/scripts/ids/IboColors.java
@@ -0,0 +1,11 @@
+package data.scripts.ids;
+
+import java.awt.Color;
+
+public class IboColors {
+
+  public static final Color IBO_COLORS = new Color(141, 31, 34, 255);
+  public static final Color GJALLARHORN = new Color(255, 255, 255, 255);
+  public static final Color TEIWAZ = new Color(83, 104, 114, 255);
+  public static final Color TEKKADAN = new Color(141, 31, 34, 255);
+}
---
+++ src/data/scripts/ids/IboHullmods.java
@@ -0,0 +1,14 @@
+package data.scripts.ids;
+
+import com.fs.starfarer.api.impl.campaign.ids.HullMods;
+
+public class IboHullmods extends HullMods {
+
+  // MODS
+  public static final java.lang.String NANOLAMINATE_ARMOR = "ibo_nano_armor";
+  public static final java.lang.String AV_SYSTEM = "ibo_av_system";
+  public static final java.lang.String CATAPULT_MS = "ibo_catapult_ms";
+  public static final java.lang.String AHAB_REACTOR = "ibo_ah_reactor";
+  // DMOD
+  public static final java.lang.String AHAB_REACTOR_SLEEP = "ibo_ah_reactor_sleep";
+}
